025ef9d27467e0bd4da3c5b01baafc7d30e2099b
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-14 15:56:21 -0300

    feat - Mappers para padronizar as comunicações utilizando os DTOs

---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs;
+using api.DTOs.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+     public static StockDto ToStockDTO(this Stock stockModel)
+     {
+        return new StockDto
+        {
+            Id = stockModel.Id,
+            Symbol = stockModel.Symbol,
+            CompanyName = stockModel.CompanyName,
+            Industry = stockModel.Industry,
+            LastDiv = stockModel.LastDiv,
+            MarketCap = stockModel.MarketCap,
+            Purchase = stockModel.Purchase,
+        };
+     }
+     public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto)
+     {
+        return new Stock
+        {
+            Symbol = stockDto.Symbol,
+            CompanyName = stockDto.CompanyName,
+            Industry = stockDto.Industry,
+            LastDiv = stockDto.LastDiv,
+            MarketCap = stockDto.MarketCap,
+            Purchase = stockDto.Purchase,
+        };
+     }
+    }
+}

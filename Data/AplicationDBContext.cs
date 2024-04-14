51ce7f24cd184832b406e984ddd0b9b169562cf3
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-14 10:16:20 -0300

    project base created

---
+++ Data/AplicationDBContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class AplicationDBContext : DbContext
+    {
+        public AplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Stock> Stock { get; set; }
+    }
+}

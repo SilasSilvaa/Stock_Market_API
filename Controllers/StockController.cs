9e2be70193c23347309a8e177030cb2838366149
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-18 00:28:33 -0300

    feat - utilização do authorize para evitar que ocorra requisições sem um token

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -4,6 +4,7 @@ using api.Mappers;
 using api.DTOs.Stock;
 using api.Interfaces;
 using api.Helpers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace api.Controllers
 {
@@ -16,20 +17,21 @@ namespace api.Controllers
         private readonly IStockRepository _repository = repository;
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
             var stocks= await _repository.GetAllAsync(query);
-            var stocksDto = stocks.Select(s => s.ToStockDTO());
-            ;
+            var stocksDto = stocks.Select(s => s.ToStockDTO()).ToList();
+
             return Ok(stocksDto);
         }
 
         [HttpGet("{id:int}")]
+        [Authorize]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
             if(!ModelState.IsValid)
@@ -49,6 +51,7 @@ namespace api.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             if(!ModelState.IsValid)
@@ -64,6 +67,7 @@ namespace api.Controllers
 
         [HttpPut]
         [Route("{id:int}")]
+        [Authorize]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStock)
         {
             if(!ModelState.IsValid)
@@ -82,6 +86,7 @@ namespace api.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
+        [Authorize]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
             if(!ModelState.IsValid)

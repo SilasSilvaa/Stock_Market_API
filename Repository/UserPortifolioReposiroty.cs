987444589df46d76d388dc7024aa91bcee0181d0
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-19 02:54:28 -0300

    feat - função para criação de um stock no portifolio

--- Repository/UserPortifolioReposiroty.cs
+++ Repository/UserPortifolioReposiroty.cs
@@ -12,6 +12,29 @@ namespace api.Repository
     public class UserPortifolioReposiroty(AplicationDBContext context) : IUserPortifolioRepository
     {
         private readonly AplicationDBContext _context = context;
+
+        public async Task<StockPortifolio> CreateAsync(StockPortifolio stockPortifolio)
+        {
+            await _context.StockPortifolios.AddAsync(stockPortifolio);
+            await _context.SaveChangesAsync();
+
+            return stockPortifolio;
+        }
+
+        public async Task<StockPortifolio> DeleteAsync(AppUser appUser, string symbol)
+        {
+            var userPortifolio = await _context.StockPortifolios.FirstOrDefaultAsync(
+                x => x.AppUser.Id == appUser.Id && x.Stock.Symbol.ToLower() == symbol.ToLower()
+                );
+ 
+            if(userPortifolio == null) return null;
+            
+            _context.StockPortifolios.Remove(userPortifolio);
+            await _context.SaveChangesAsync();
+
+            return userPortifolio;
+        }
+
         public async Task<List<Stock>> GetUserPortifolio(AppUser user)
         {
             return await _context.StockPortifolios.Where(x => x.AppUserId == user.Id)

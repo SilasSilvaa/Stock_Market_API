9a7125bd8c1a8a2fada28950c8ea9126bb6c4a0e
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-20 03:26:12 -0300

    feat - função para buscar todas as ações da api

--- Interfaces/IFinantialModPreparingService.cs
+++ Interfaces/IFinantialModPreparingService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
@@ -9,5 +10,6 @@ namespace api.Interfaces
     public interface IFinantialModPreparingService
     {
         Task<Stock> FindByStockBySymbolAsync(string symbol);
+        Task<List<StockList>> GetAllStocks(QueryApiObject query);
     }
 }

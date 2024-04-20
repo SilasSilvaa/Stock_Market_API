0311441d7f4ec32bef25c4c291f16626f5260929
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-20 03:27:58 -0300

    feat - função para busca de ações na api e paginação

--- Service/FinantialModPreparingService.cs
+++ Service/FinantialModPreparingService.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using api.DTOs;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Newtonsoft.Json;
 
 namespace api.Service
@@ -25,20 +27,54 @@ namespace api.Service
             {
                 var content = await result.Content.ReadAsStringAsync();
                 var contentDeserialize = JsonConvert.DeserializeObject<FMPStock[]>(content);
+                
+                if(contentDeserialize != null && contentDeserialize.Length > 0){
+
                 var stock = contentDeserialize[0];
 
                 if(stock != null)
                 {
                     return stock.ToStockFromFMP();
                 }
-                return null;             
-            }   
-            return null;
+                    throw new JsonException("Error to deserialize json");             
+                }
+
+            }
+            throw new HttpRequestException("Error to request data");
          
-         }  catch (Exception e) {
-            Console.WriteLine(e);
-            return null;
+         }  catch (Exception) {
+            throw new Exception();
          } 
         }
+
+        public async Task<List<StockList>> GetAllStocks(QueryApiObject query)
+        {
+            try
+            {
+                var result = await _httpClient.GetAsync($"https://financialmodelingprep.com/api/v3/stock/list?apikey={_configuration["APIKey"]}");
+                
+                if(result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+                    var simpleStockList = JsonConvert.DeserializeObject<List<StockList>>(content) ?? throw new Exception("Error");
+
+                    var skipNumber = (query.PageNumber -1 ) * query.PageSize;
+                    List<StockList> stockList = simpleStockList.ToSimpleStockList().Skip(skipNumber).Take(query.PageSize).ToList();
+                    
+                    return stockList;                    
+                }
+                else
+                {
+                    throw new Exception("Erro to get data");
+                }   
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception(e.Message);
+            }
+        }
+
     }
 }

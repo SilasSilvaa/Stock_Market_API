ba38b519e6d467db50961d679ab60800215048e4
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-21 20:24:20 -0300

    refact - alteração nas queries

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace api.Helpers
 {
     public class QueryObject
     {
-        public string? Symbol { get; set; } = null;
-        public string? CompanyName { get; set; } = null;
-        public string? Industry { get; set; } = null;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        public bool OrderBySymbol { get; set; } = false;
         public bool OrderByName { get; set; } = false;
+        public bool OrderByPrice { get; set; } = false;
+        public bool OrderByStockExchange { get; set; } = false;
+        public bool OrderByExchangeShortName { get; set; } = false;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 20;
 

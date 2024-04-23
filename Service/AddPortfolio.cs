82dfbf998d3b311a0259af4852bffd588bb15e06
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-23 01:51:20 -0300

    feat - class para validação dos dados recebidos via body

---
+++ Service/AddPortfolio.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Service
+{
+    public class AddPortfolio
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ Service/DeletePortfolio.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Service
+{
+    public class DeletePortfolio
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+    }
+}

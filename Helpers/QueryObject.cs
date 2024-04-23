9c98aa663efcbb538663ee6e5da2edab07258156
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-23 01:47:40 -0300

    reactor - remoção de prop não utilizada

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace api.Helpers
 {
     public class QueryObject
     {
-        [Required]
-        public string Email { get; set; } = string.Empty;
         public bool OrderBySymbol { get; set; } = false;
         public bool OrderByName { get; set; } = false;
         public bool OrderByPrice { get; set; } = false;
-        public bool OrderByStockExchange { get; set; } = false;
-        public bool OrderByExchangeShortName { get; set; } = false;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 20;
 

d482d2a15a0f2b2f52df3db7467a45843e71d7b7
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-23 01:49:34 -0300

    feat - repository responsvel por realizar as alterações no banco de dados

---
+++ Repository/StockDBRepository.cs
@@ -0,0 +1,96 @@
+using api.Data;
+using api.DTOs.Stock;
+using api.Helpers;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class StockDBRepository(AplicationDBContext context, UserManager<AppUser> manager) : IStockDBRepository
+    {
+        private readonly AplicationDBContext _context = context;
+        private readonly  UserManager<AppUser> _manager = manager;
+
+        // private readonly IFinantialModPreparingService _service = service;
+
+        public async Task<List<StockDB>> GetAllAsync(QueryObject query)
+        {
+            try
+            {
+                var stocks = _context.Stock.AsQueryable();
+                
+                if(query.OrderByName) stocks = stocks.OrderBy(s => s.CompanyName);
+                if(query.OrderByPrice) stocks = stocks.OrderBy(s => s.Price);
+                if(query.OrderBySymbol) stocks = stocks.OrderBy(s => s.Symbol);
+                
+                var skipNumber = (query.PageNumber -1 ) * query.PageSize;
+                return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<StockDB?> GetByIdAsync(int id)
+        {
+            return await _context.Stock.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<StockDB> CreateAsync(StockDB stock)
+        {
+            await _context.Stock.AddAsync(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<StockDB?> UpdateAsync(int id, UpdateStockRequestDto updateStock)
+        {
+             var existingStock = await _context.Stock.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingStock == null)
+            {
+                return null;
+            }
+            
+            existingStock.Symbol = updateStock.Symbol;
+            existingStock.CompanyName = updateStock.CompanyName;
+            existingStock.Price = updateStock.Price;
+            existingStock.LastDiv = updateStock.LastDiv;
+            existingStock.Industry = updateStock.Industry;
+            existingStock.MarketCap = updateStock.MarketCap;
+            existingStock.Changes = updateStock.Changes;
+            existingStock.Currency = updateStock.Currency;
+            existingStock.Description = updateStock.Description;
+            existingStock.Image = updateStock.Image;
+
+            await _context.SaveChangesAsync();
+
+            return existingStock;
+        }
+
+        public async Task<StockDB> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stock.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return null;
+            }
+
+            _context.Stock.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<StockDB?> GetBySymbolAsync(string symbol)
+        {
+            return await _context.Stock.FirstOrDefaultAsync(s => s.Symbol == symbol);
+        }
+    }
+}

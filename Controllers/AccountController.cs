0d61dc951f964a2d7d5e33846d63391830033996
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-23 01:43:38 -0300

    refact - alteração no nome da prop e remoção de importação não utilizada

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.DTOs.Account;
 using api.Interfaces;
 using api.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;

bbaa2882e156e5398ee9e4807a667b78212be3a0
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-23 01:49:00 -0300

    feat - novas props para ações

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -7,17 +7,22 @@ using System.Threading.Tasks;
 namespace api.Models
 {
     [Table("Stocks")]
-    public class Stock
+    public class StockDB
     {
         public int Id { get; set; }
-        public string? UserId { get; set; }
         public string Symbol { get; set; } = string.Empty;
-        public string? Image { get; set; } = string.Empty;
-        public string Name { get; set;} = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string CompanyName { get; set;} = string.Empty;
         [Column(TypeName = "decimal(18,2)")]
-        public decimal? Price { get; set; }
-        public string? StockExchange { get; set; } = string.Empty;
-        public string? ExchangeShortName { get; set; } = string.Empty;        
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Changes { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public long MarketCap { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Industry { get; set; } = string.Empty;
         public List<StockPortifolio> Portifolio { get; set; } = new List<StockPortifolio>();
 
     }

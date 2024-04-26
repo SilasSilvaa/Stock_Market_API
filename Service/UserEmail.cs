d87d57068c5a3fc9034c6c15943ca51687762000
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2024-04-25 23:19:39 -0300

    feat - class de validação de email via body

---
+++ Service/UserEmail.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Service
+{
+    public class UserEmail
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+}

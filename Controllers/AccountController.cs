990723e0b4ea139724935b13d4dceedb7e76e1e5
SilasSilvaa <silas-oli@hotmail.com>
2024-10-19 11:34:02 -0300

    refact - adicionando validação de e-mail

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,7 +10,7 @@ namespace api.Controllers
 {
 
     [Route("api/account")]
-    [ApiController] 
+    [ApiController]
     public class AccountController(UserManager<AppUser> userManager, ITokenService token, SignInManager<AppUser> signInManager) : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager = userManager;
@@ -20,43 +20,56 @@ namespace api.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] Register register)
         {
-            try{
+            try
+            {
 
-                if(!ModelState.IsValid) return BadRequest(ModelState);    
-                
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                if (register.Email != null)
+                {
+                    var existingUserByEmail = await _userManager.FindByEmailAsync(register.Email);
+                    if (existingUserByEmail != null)
+                    {
+                        return BadRequest("Email already exists.");
+                    }
+                }
+
                 var appUser = new AppUser
                 {
                     UserName = register.UserName,
                     Email = register.Email,
                 };
 
-                if(register.Password == null) return BadRequest("Error password cannot be null.");
+                if (register.Password == null) return BadRequest("Error password cannot be null.");
                 var createdUser = await _userManager.CreateAsync(appUser, register.Password);
-             
-                if(createdUser.Succeeded)
+
+                if (createdUser.Succeeded)
                 {
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
 
-                    if(roleResult.Succeeded)
+                    if (roleResult.Succeeded)
                     {
                         return Ok(
-                            new User 
+                            new User
                             {
                                 UserName = register.UserName,
                                 Email = register.Email,
                                 Token = _token.CreateToken(appUser)
                             }
                         );
-                    }else
+                    }
+                    else
                     {
                         return StatusCode(500, roleResult.Errors);
                     }
-                }else 
+                }
+                else
                 {
                     return StatusCode(500, createdUser.Errors);
                 }
 
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
                 return StatusCode(500, e);
             }
@@ -66,20 +79,20 @@ namespace api.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] Login login)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if(login.Email == null) return BadRequest("Error email cannot be null.");
+            if (login.Email == null) return BadRequest("Error email cannot be null.");
             var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Email == login.Email.ToLower());
 
-            if(user == null) return Unauthorized("Invalid E-mail");
+            if (user == null) return Unauthorized("Invalid E-mail");
 
-            if(login.Password == null) return BadRequest("Error password cannot be null.");
+            if (login.Password == null) return BadRequest("Error password cannot be null.");
             var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
 
-            if(!result.Succeeded) return Unauthorized("E-mail not found or your password is wrong");
+            if (!result.Succeeded) return Unauthorized("E-mail not found or your password is wrong");
 
             return Ok(
                 new User
@@ -91,27 +104,28 @@ namespace api.Controllers
             );
 
         }
-    
+
         [HttpPost("logout")]
         public async Task<IActionResult> Logout()
         {
             try
             {
-            await _signInManager.SignOutAsync();
-            return Ok("User logged out successfully.");
-            }catch (Exception)
+                await _signInManager.SignOutAsync();
+                return Ok("User logged out successfully.");
+            }
+            catch (Exception)
             {
                 throw;
             }
 
         }
-    
+
         [HttpPost("getrole")]
         [Authorize]
         public async Task<IActionResult> GetRoleByUser([FromBody] UserEmail user)
         {
             var appUser = await _userManager.FindByEmailAsync(user.Email);
-            if(appUser == null) 
+            if (appUser == null)
             {
                 return BadRequest("User not found");
             }
